968ed66681761c0694c99c6251de7887199ed517
Afiqah Rashid <aficatrashid@gmail.com>
2019-07-26 17:39:00 +0800

    updated with npm deploy

--- next.config.js
+++ next.config.js
@@ -1,1 +1,20 @@
-module.exports = {};
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  exportPathMap: async function(defaultPathMap) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  assetPrefix: !debug ? '/card-display-page/' : '',
+  webpack: (config, { dev }) => {
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    // Important: return the modified config
+    return config
+  }
+}
